4156718f76dec3751dcc2e4b86d191815d8f0966
Jorge <jorge_antonio_sistema@hotmail.com>
2026-06-03 13:39:57 -0600

    Prueba técnica finalizada. Lógica y Estructura terminadas.

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, Pressable } from "react-native";
+import { StyleSheet } from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const [contador, setContador] = useState(0);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.contenedor}>
+      {/* Boton para decrementar el contador, con una alerta si el contador es menor o igual a cero. */}
+      <Pressable
+        onPress={() => {
+          if (contador <= 0) {
+            alert("El contador no puede ser menor a cero.");
+          } else {
+            setContador(contador - 1);
+          }
+        }}
+        style={styles.presionableDecrementar}  
+      >
+        <Text style={styles.textoBoton}>Decrementar</Text>
+      </Pressable>
+      {/* Texto que muestra el valor del contador. */}
+      <Text style={styles.textoContador}>{contador}</Text>
+      {/* Boton para incrementar el contador. */}
+      <Pressable
+        onPress={() => {
+          setContador(contador + 1);
+        }}
+        style={styles.presionableAumentar}
+      >
+        <Text style={styles.textoBoton}>Incrementar</Text>
+      </Pressable>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  contenedor: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#e0e0e0",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row"
   },
+    presionableAumentar: {
+      backgroundColor: "#4CAF50",
+      padding: 5,
+      borderRadius: 5,
+      paddingVertical: 20,
+    },
+    presionableDecrementar: {
+      backgroundColor: "#f44336",
+      padding: 5,
+      borderRadius: 5,
+      paddingVertical: 20,
+    },
+  textoBoton: {
+    fontSize: 20,
+    fontWeight: "semibold",
+    userSelect: "none"
+  },
+  textoContador: {
+    fontSize: 40,
+    fontWeight: "bold",
+    marginHorizontal: 20
+  }
 });
